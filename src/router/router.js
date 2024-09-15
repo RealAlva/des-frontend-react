951363c21f0526bb36f8d9f5043a1519368bb996
Freddy Daniel Alvarez <falvarezpinto@gmail.com>
2024-09-15 19:39:54 -0400

    some commit

--- src/router/router.js
+++ src/router/router.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Product from "../screens/Product";
-import LoginForm from "../screens/Forms/LoginForm";
-import MainPage from "../components/MainPage";  // Importa el componente MainPage
-import DictionaryScreen from '../screens/dictionary/DictionaryScreen'; // Importamos el componente
-
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import DictionaryScreen from '../screens/dictionary/DictionaryScreen';
+import ProductsScreen from '../screens/Product';
+import LoginForm from '../screens/Forms/LoginForm';
+import MainPage from '../components/MainPage';
+import Navbar from '../components/Navbar';
 
 const RouterConfig = () => {
   return (
-    <BrowserRouter basename="/des-frontend-react"> {/* Add basename */}
+    <HashRouter basename="/des-frontend-react">
+      <Navbar />
       <Routes>
-        {/* Update the default route to MainPage */}
         <Route path="/" element={<MainPage />} />
         <Route path="/main" element={<MainPage />} />
-        <Route path="/products" element={<Product />} />
+        <Route path="/products" element={<ProductsScreen />} />
         <Route path="/login" element={<LoginForm />} />
-        <Route path="/dictionary" element={<DictionaryScreen />} /> {/* Nueva ruta */}
-
+        <Route path="/dictionary" element={<DictionaryScreen />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
