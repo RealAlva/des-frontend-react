d62938b2456319bb2c4b8d07dec4dca4f4dee5a8
Freddy Daniel Alvarez <falvarezpinto@gmail.com>
2024-09-15 20:58:10 -0400

    Updates

--- src/redux/default/defaultActions.js
+++
@@ -1,8 +0,0 @@
-import { SET_NAME_MODULE } from './defaultTypes';
-
-export const setNameModule = (nameModule) => {
-    return {
-        type: SET_NAME_MODULE,
-        payload: nameModule,
-    }
-}
